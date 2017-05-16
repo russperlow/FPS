2e83e80ca1a48bc7df4742a16a5bf19cf99269ff
Russell Perlow (RIT Student) <rep4975@ad.rit.edu>
2017-05-16 16:20:42 -0400

    Lots of external files added, character created with a gun, gun shoots and bullets work with collision

---
+++ FPS/Assets/Scripts/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	// Public Variables
+	public GameObject player;
+	public Camera mainCamera;
+	public Camera scopeCamera;
+
+	// Use this for initialization
+	void Start () {
+		scopeCamera.enabled = false;
+		mainCamera.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (1)) 
+		{
+			scopeCamera.enabled = true;
+			mainCamera.enabled = false;
+		} 
+		else 
+		{
+			scopeCamera.enabled = false;
+			mainCamera.enabled = true;
+		}
+	}
+}
---
+++ FPS/Assets/Scripts/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    // Public Variables
+    public GameObject bullet;
+    public float force = 3000;
+    public float waitTime = .1f;
+
+    // Private Variables
+    public float timeInterval = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeInterval += Time.deltaTime;
+        if (Input.GetMouseButton(0) && timeInterval >= waitTime)
+        {
+            GameObject temp = (GameObject)GameObject.Instantiate(bullet);
+            temp.transform.position = this.transform.position;
+            temp.transform.rotation = this.transform.rotation;
+            temp.transform.eulerAngles = new Vector3(this.transform.rotation.x + 90, this.transform.rotation.y, this.transform.rotation.z);
+            temp.GetComponent<Rigidbody>().AddForce(this.transform.forward * force);
+            BulletManager bm = (BulletManager)temp.GetComponent(typeof(BulletManager));
+            bm.enabled = true;
+            timeInterval = 0;
+        }
+	}
+}
